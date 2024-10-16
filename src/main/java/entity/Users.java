142f12dbc44952b2f5a22d2a05e1456e83372d62
ChelleRech3 <vutdhe181303@fpt.edu.vn>
2024-10-16 13:46:29 +0700

    fix loi typo 16/10

--- src/main/java/entity/Users.java
+++ src/main/java/entity/Users.java
@@ -25,8 +25,8 @@ public class Users {
         private String gender;
 
     // Role constants
-      public static final int ROLE_STUDENT = 1;
-       public static final int ROLE_TEACHER = 2;
+      public static final int ROLE_STUDENT = 2;
+       public static final int ROLE_TEACHER = 1;
       public static final int ROLE_ADMIN = 3;
 
         public Users() {
